eaf4dfccbff55db79eb58203b65da4c58a726423
Denis Bonaccini <dbonaccini8@gmail.com>
2024-11-01 22:03:11 -0300

    push

--- src/app/components/PortfolioSection.jsx
+++ src/app/components/PortfolioSection.jsx
@@ -12,10 +12,10 @@ export default function PortfolioSection() {
       return (
         <section 
           id="tecnologias" style={{ borderBottomWidth:'0.1px'}} 
-          className="h-screen flex flex-col justify-center items-center bg-[url('/bg-1.png')] bg-fixed bg-brandBlack/20 shadow-2xl border-brandWhite shadow-brandWhite/80 text-center text-brandWhite"
+          className="h-auto py-24 flex flex-col justify-center items-center bg-[url('/bg-1.png')] bg-fixed bg-brandBlack/20 shadow-2xl border-brandWhite shadow-brandWhite/80 text-center text-brandWhite"
         >
           <h2 className="text-3xl font-heading text-center mb-12">Tecnologias Utilizadas</h2>
-          <div className="grid mt-12 grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid mx-2 mt-12 grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {technologies.map((tech, index) => (
               <div key={index} className="flex flex-col items-center">
                 <img src={tech.icon} alt={tech.name} className="w-28 h-28 sm:w-32 sm:h-32 lg:w-44 lg:h-44 mb-4 rounded-xl shadow-lg shadow-brandBlue/80"  style={{ borderWidth: '0.10px' }} />
--- src/app/components/Vrz.jsx
+++ src/app/components/Vrz.jsx
@@ -4,52 +4,50 @@ import { useState, useEffect } from "react";
 
 function Vrz() {
   const [frameIndex, setFrameIndex] = useState(0);
-  const totalFrames = 210;
+  const totalFrames = 151;
 
   useEffect(() => {
     const interval = setInterval(() => {
       setFrameIndex((prevIndex) => (prevIndex + 1) % totalFrames);
-    }, 20);
+    }, 40);
 
     return () => clearInterval(interval);
   }, []);
 
-  const framePath = `/frames/Motion_VRZ_Completa_${String(frameIndex).padStart(5, "0")}.png`;
+  const framePath = `/frames/frame-int/Motion_Logotipo${String(frameIndex).padStart(5, "0")}.png`;
 
   return (
-    <div className="relative lg:h-[1400px] h-screen w-screen overflow-hidden bg-[url('/bg.png')] bg-cover bg-center flex flex-col items-center justify-start pt-8 md:pt-16">
+    <div className="relative lg:h-[1400px] h-[1200px] w-auto overflow-hidden bg-[url('/bg.png')] bg-cover bg-center flex flex-col items-center justify-start pt-8 md:pt-16">
       
       {/* Imagem de Fundo Fixa no Lado Direito Inferior */}
-      <div className="absolute absolute animate-space-drift bottom-0 right-0 lg:h-[600px] lg:w-[600px] md:h-[400px] md:w-[400px] h-[300px] w-[300px] bg-contain bg-no-repeat opacity-80 pointer-events-none">
+      <div className="absolute animate-space-drift bottom-0 right-0 lg:h-[600px] md:h-[400px] h-[300px] bg-contain bg-no-repeat opacity-40 pointer-events-none md:mt-0 w-80 sm:w-64 md:w-80 lg:w-[600px]">
       <Image alt="lua" src="/lua.png" width={340} height={160} className="" />
       </div>
 
       {/* Container de texto e imagem */}
       <div className="relative z-10 flex flex-col md:grid md:grid-cols-2 items-center md:gap-4 text-center md:text-left text-brandWhite mt-8 md:mt-12 max-w-screen md:ml-20 mx-6 lg:ml-28">
-        
-        <div className="flex flex-col items-center md:items-start md:mx-6 lg:mx-8">
-          <h1 className="text-3xl md:text-6xl font-bold mb-4">Conheça a VRZ Studio</h1>
-          <p className="text-lg md:text-2xl mt-4 md:mt-6 mx-8 md:mx-0">
-            Saia do mundo da Lua e conquiste o seu espaço no mundo digital. Fornecemos soluções reais para negócios reais.
-          </p>
-        </div>
-        
-        <div className="mt-6 md:mt-12 flex justify-center md:justify-end">
-          <Image src="/back-hero.png" alt="hero" width={320} height={210} className="animate-bounce mt-10 md:mt-0 w-80 sm:w-64 md:w-80 lg:w-[700px]"  />
-        </div>
-      </div>
-
-      {/* Animação da logo abaixo de todo o conteúdo */}
-      <div className="absolute bottom-4 sm:bottom-16 md:bottom-16 lg:bottom-16 w-full flex items-center justify-center">
+        <div className="flex flex-col items-center md:items-start md:mx-6 lg:mx-8 ">
         <Image
           src={framePath}
           alt="Animação da logo"
           width={360}
           height={200}
           priority
-          className="w-44 sm:w-1/2 md:w-1/3 lg:w-1/4" // Ajustando a largura para diferentes breakpoints
+          className="w-[260px] sm:w-[300px] mb-2 md:w-[360px] lg:w-[400px] md:ml lg:ml-44" // Ajustando a largura para diferentes breakpoints
         />
+          
+        </div>
+        
+        <div className="mt-6 md:mt-6 flex justify-center">
+          <Image src="/back-hero.png" alt="hero" width={320} height={210} className="animate-bounce mt-10 md:ml-10 lg:ml-14 md:mt-0 w-80 sm:w-80 md:w-80 lg:w-[600px] lg:mr-16"  />
+        </div>
+        <p className="text-base  sm:relative sm:text-xl md:text-2xl lg:text-3xl md:ml-20 lg:ml-20 mx-8">
+        A <span className="font-black text-brandWhite">BORLENZ</span><span className="font-light text-brandOrange">®</span> tem orgulho de ter a <span className="font-black text-brandGreen">VRZ Software House </span> como parceira estratégica, especialista em soluções digitais inovadoras. A VRZ oferece um sistema de <span className="text-brandVrz font-black">CRM</span> Empresarial que centraliza dados para melhor gestão de leads e experiência do cliente, além de implementar chatbots com <span className="font-black text-brandVrz">IA</span> para interações rápidas e personalizadas. Também desenvolve sites institucionais otimizados para SEO e com excelente experiência do usuário. Com foco em qualidade e inovação, a VRZ ajuda as empresas a melhorar sua presença online e transformar interações em resultados concretos.
+        </p>
       </div>
+
+      {/* Animação da logo abaixo de todo o conteúdo */}
+     
 
       
     </div>
