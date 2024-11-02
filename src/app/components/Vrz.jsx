091d360e29d2eaf62aa5485c7d3cbd700c4ef3f3
Denis Bonaccini <dbonaccini8@gmail.com>
2024-11-02 13:37:26 -0300

    push

--- src/app/components/Vrz.jsx
+++ src/app/components/Vrz.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 function Vrz() {
   const [frameIndex, setFrameIndex] = useState(0);
-  const totalFrames = 151;
+  const totalFrames = 150;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -14,13 +14,13 @@ function Vrz() {
     return () => clearInterval(interval);
   }, []);
 
-  const framePath = `/frames/frame-int/Motion_Logotipo${String(frameIndex).padStart(5, "0")}.png`;
+  const framePath = `/frames/Motion_VRZ_Completa_${String(frameIndex).padStart(5, "0")}.png`;
 
   return (
-    <div className="relative lg:h-[1400px] h-[1200px] w-auto overflow-hidden bg-[url('/bg.png')] bg-cover bg-center flex flex-col items-center justify-start pt-8 md:pt-16">
+    <div className="relative lg:h-auto h-auto w-auto overflow-hidden bg-[url('/bg.png')] bg-cover bg-center flex flex-col items-center justify-start pt-8 md:pt-16">
       
       {/* Imagem de Fundo Fixa no Lado Direito Inferior */}
-      <div className="absolute animate-space-drift bottom-0 right-0 lg:h-[600px] md:h-[400px] h-[300px] bg-contain bg-no-repeat opacity-40 pointer-events-none md:mt-0 w-80 sm:w-64 md:w-80 lg:w-[600px]">
+      <div className="absolute animate-space-drift bottom-0 right-0 lg:h-[600px] md:h-[600px] h-[500px] bg-contain bg-no-repeat opacity-40 pointer-events-none md:mt-0 w-80 sm:w-64 md:w-80 lg:w-[600px]">
       <Image alt="lua" src="/lua.png" width={340} height={160} className="" />
       </div>
 
@@ -39,17 +39,24 @@ function Vrz() {
         </div>
         
         <div className="mt-6 md:mt-6 flex justify-center">
-          <Image src="/back-hero.png" alt="hero" width={320} height={210} className="animate-bounce mt-10 md:ml-10 lg:ml-14 md:mt-0 w-80 sm:w-80 md:w-80 lg:w-[600px] lg:mr-16"  />
+          <Image src="/back-hero.png" alt="hero" width={320} height={210} className="animate-bounce mt-10 md:ml-10 lg:ml-14 md:mt-0 w-80 sm:w-80 md:w-80 lg:w-[500px] lg:mr-16"  />
         </div>
-        <p className="text-base  sm:relative sm:text-xl md:text-2xl lg:text-3xl md:ml-20 lg:ml-20 mx-8">
+        <p className="text-base  sm:relative sm:text-xl md:text-xl lg:text-3xl md:ml-20 lg:ml-20 mx-8">
         A <span className="font-black text-brandWhite">BORLENZ</span><span className="font-light text-brandOrange">®</span> tem orgulho de ter a <span className="font-black text-brandGreen">VRZ Software House </span> como parceira estratégica, especialista em soluções digitais inovadoras. A VRZ oferece um sistema de <span className="text-brandVrz font-black">CRM</span> Empresarial que centraliza dados para melhor gestão de leads e experiência do cliente, além de implementar chatbots com <span className="font-black text-brandVrz">IA</span> para interações rápidas e personalizadas. <br/><br/> Também desenvolve sites institucionais otimizados para SEO e com excelente experiência do usuário. Com foco em qualidade e inovação, a VRZ ajuda as empresas a melhorar sua presença online e transformar interações em resultados concretos.
         </p>
       </div>
 
-      {/* Animação da logo abaixo de todo o conteúdo */}
-     
-
       
+      <div className="flex justify-center items-center pt-10 lg:mt-4 xl:mt-4 mb-20 ">
+        <Image 
+          src="/tag-2.png" 
+          alt="hero" 
+          width={140} 
+          height={60} 
+          className="rounded-xl w-32 sm:w-40 md:w-48 lg:w-56 xl:w-56 lg:xl:mt-10"
+        />
+      </div>
+
     </div>
   );
 }
